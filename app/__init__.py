215f1d02cd71aa222a3bd8cf2c8cafb0bb2e916f
kelly kiiru <kelly.kiiru@student.moringaschool.com>
2022-05-20 10:09:49 +0300

    add login page

--- app/__init__.py
+++ app/__init__.py
@@ -6,7 +6,6 @@ from flask_bootstrap import Bootstrap
 db=SQLAlchemy()
 bootstrap=Bootstrap()
 login_manager = LoginManager()
-bootstrap=Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__)
